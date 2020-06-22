e366baeccc52787e202b77f10d7442896b0b68fd
lucic71 <lucian.popescu187@gmail.com>
2020-06-22 18:16:12 +0300

    Added memory io library functions

--- lib/include/memio.h
+++ lib/include/memio.h
@@ -1,6 +1,17 @@
 #ifndef IO_H_
 #define IO_H_
 
-void mio_out(unsigned short port, unsigned char data);
+#include "stdint.h"
+
+/*
+ * outb:
+ *  Writes the second parameter to a I/O port specified in the first parameter.
+ *
+ * @param port - I/O port where data is written
+ * @param data - data to be written to the port
+ *
+ */
+
+void outb(uint16_t port, uint8_t data);
 
 #endif
---
+++ lib/memio/asm/memio.s
@@ -0,0 +1,9 @@
+global mio_out
+
+outb:
+
+    mov al, [esp + 8]
+    mov dx, [esp + 4]
+
+    out dx, al
+    ret
