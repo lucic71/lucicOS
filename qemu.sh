3494e213804d166bde51819a410cc26a1e886309
lucic71 <lucian.popescu187@gmail.com>
2020-07-03 16:27:29 +0300

    Added debug settings for qemu

--- qemu.sh
+++ qemu.sh
@@ -2,4 +2,8 @@
 set -e
 . ./iso.sh
 
-qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom os.iso
+if [ "$1" = "debug" ]; then
+    qemu-system-$(./target-triplet-to-arch.sh $HOST) -s -S -cdrom os.iso
+else
+    qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom os.iso
+fi
