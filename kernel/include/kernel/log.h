c79344940221d4b93441494eb0a5467fd5933172
lucic71 <lucian.popescu187@gmail.com>
2020-07-06 19:05:37 +0300

    Changed declaration of reg_dump to match kb shortcut handler

--- kernel/include/kernel/log.h
+++ kernel/include/kernel/log.h
@@ -7,8 +7,12 @@
  * reg_dump:
  *  Dumps the content of general purpose registers on the screen.
  *
+ * Argument became void * because this routine is used as a handeler for a 
+ * keyboard shortcut that receives a pointer to void. Normally I would set
+ * the argument to void.
+ *
  */
 
-void reg_dump(void);
+void reg_dump(void *);
 
 #endif
