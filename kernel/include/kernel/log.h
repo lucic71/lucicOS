ed9bf10abad963b6c843b4b4d15c79247bf3cb78
lucic71 <lucian.popescu187@gmail.com>
2020-07-06 15:46:53 +0300

    Implemented register dump routine

---
+++ kernel/include/kernel/log.h
@@ -0,0 +1,14 @@
+#ifndef LOG_H_
+#define LOG_H_
+
+#include <stdint.h>
+
+/*
+ * reg_dump:
+ *  Dumps the content of general purpose registers on the screen.
+ *
+ */
+
+void reg_dump(void);
+
+#endif
