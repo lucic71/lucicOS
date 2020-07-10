853a93645027ca158162cd9ef01d23c223e4e764
lucic71 <lucian.popescu187@gmail.com>
2020-07-10 16:20:52 +0300

    Updated kernel.c and Makefile

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -7,35 +7,50 @@
 #include "kernel/multiboot.h"
 #include "kernel/log.h"
 #include "kernel/pmm.h"
+#include "kmain/multiboot.h"
 
 #include "i386/isr.h"
 #include "i386/irq.h"
 #include "i386/idt.h"
 
+
 void kmain(multiboot_info_t *mb_info, uint32_t mb_magic) {
+
+    /* Interrupt initialization. */
 
     idt_init();
     irq_init();
     isr_init();
 
+    /* Screen initialization. */
+
     screen_init();
 
-    if (mb_magic != MULTIBOOT_BOOTLOADER_MAGIC)
-        panic("Not a multiboot bootloader!");
+    /* Multiboot initialization. */
 
-    if (!mb_info)
-        panic("Invalid multiboot pointer");
+    multiboot_init(mb_info, mb_magic);
 
-    if (!(mb_info->flags & MULTIBOOT_INFO_MEMORY))
-        panic("There is no memory info in multiboot");
+    /* Physical memory manager initialization. */
 
-    printf("mem_lower: 0x%x B, mem_upper: 0x%x B\n\n", mb_info->mem_lower * 1024, mb_info->mem_upper * 1024);
-    print_memory_map((multiboot_memory_map_t *) mb_info->mmap_addr,
-            (multiboot_memory_map_t *) (mb_info->mmap_addr + mb_info->mmap_length));
+    print_memory_map(mb_info->mmap_addr,
+             mb_info->mmap_addr + mb_info->mmap_length);
     puts("");
     print_kernel_map();
 
+    uint32_t mem_size = 1024 + mb_info->mem_upper;
+    extern uint8_t *_kernel_end;
+
+    pmm_init((uint32_t) &_kernel_end, mem_size);
+
+    pmm_init_available_regions(mb_info->mmap_addr,
+            mb_info->mmap_addr + mb_info->mmap_length);
+    pmm_deinit_kernel();
+
+    /* Serial initialiation. */
+
     serial_init(COM1);
+
+    /* Keyboard initialiation. */
 
     kb_init();
 
